f3dc1975d8e312970eae6a47195067e3efb07116
Terror1121 <kykymber601@gmail.com>
2026-06-28 13:15:33 +0300

    Update RobloxCheats.lua

--- RobloxCheats.lua
+++ RobloxCheats.lua
@@ -5,12 +5,14 @@ local Window = Rayfield:CreateWindow({
     Name = "Моё крутое меню",
     LoadingTitle = "Загрузка...",
     LoadingSubtitle = "by YourName",
+    ScriptID = "sid_eo08v93jcdta",
     -- Клавиша для открытия/закрытия меню
     ToggleUIKeybind = Enum.KeyCode.K,
+    
 })
 
 -- 3. Создаем вкладку
-local Tab = Window:CreateTab("Основное", 4483362458, ScriptID = "sid_eo08v93jcdta",) -- Второй аргумент — иконка
+local Tab = Window:CreateTab("Основное", 4483362458) -- Второй аргумент — иконка
 
 -- 4. Создаем секцию внутри вкладки
 local Section = Tab:CreateSection("Настройки")

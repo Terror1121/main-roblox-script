75f8b9c60e2085a640c3d3c14d89dc8209456c56
Terror1121 <kykymber601@gmail.com>
2026-06-30 10:23:16 +0300

    alfa-001-patch003

--- RobloxCheats.lua
+++ RobloxCheats.lua
@@ -23,7 +23,7 @@ local SectionInfo = TabInf:CreateSection("О чите")
 
 local InfoParagraph = TabInf:CreateParagraph({
     Title = "Информация",
-    Content = "Сделано разработчиком namesick\nВерсия alfa-001-patch002",
+    Content = "Сделано разработчиком namesick\nВерсия alfa-001-patch003",
 })
 
 -- ============================================
